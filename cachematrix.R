9ba9848a2cece431a15442e8510ad8b8de4bf46d
rtorlow <rtorlo@BSAD-39FKZ42.gfk.com>
2015-09-24 15:12:56 +0200

    solution to assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix is a function that stores a list of 
+## functions
 
-## Write a short comment describing this function
-
+## function makeCacheMatrix stores some functions:
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y               # "super" assignment operator
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  
+  #the following line stores the functions:
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve verify the value m, stored previously 
+## with getmean, If it exists in memory, it simply 
+## returns a message and the value m, otherwise it
+## calculates the inverse of 'x' created 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getmean()
+      if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmean(m)
+  m
 }
